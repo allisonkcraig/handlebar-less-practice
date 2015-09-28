d6c1d012e2842515b41fb3ad8e55c35367ca8e74
Allison  Craig <allison@craig.net>
2015-09-28 12:10:10 -0700

    getting the hang of LESS

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,31 @@
-var compliments = ['You are amazing!','Hello Beautiful!']
+(function(){
+	var blogPosts = [
+		{title:"TwitchCon!", body:"It was so much fun!!", img:"http://www.3dvalley.com/tutorialsdata/flower.png"},
+		{title:"Hackbright", body:"HEYY", img:"http://www.3dvalley.com/tutorialsdata/flower.png"},
+		{title:"About Me", body:"I am tall.", img:"http://www.3dvalley.com/tutorialsdata/flower.png"},
+	];
+
+	var template = Handlebars.compile( $('#entry-template').html() );
+			for (var i = 0; i < blogPosts.length; i++){
+				$('.blog-content').append(template(blogPosts[i]));
+	};
+})();
+
+(function(){
+	var compliments =  ['Hello, you beautiful, rule-breaking moth.',
+		'Hello, you cunning, pliable, chestnut-haired sunfish.',
+		'Hello, you poetic, noble land-mermaid.',
+		'Hello, you beautiful, naive, sophisticated newborn baby.',
+		"Hello, you beautiful tropical fish. You're smart as a whip and you're cool under pressure."];
 
 	var randPick = Math.floor( Math.random() * compliments.length );
-	console.log(randPick)
 
+	var data = {
+		compliment: compliments[randPick]};		
 
+	var template = Handlebars.compile( $('#subtitle-template').html() );
+	var html = template(data);
 
+	$('#subtitle').append(template(data));
+})();
 
-	(function(){
-			var data = {
-				title:"Allison's blog",
-				body:compliments[randPick]
-			};
-			console.log(data[body])
-
-			var template = Handlebars.compile( $('#entry-template').html() );
-			console.log(template);
-			var html = template(data);
-			console.log(html);
-			$('body').append(template(data));
-
-	})();
